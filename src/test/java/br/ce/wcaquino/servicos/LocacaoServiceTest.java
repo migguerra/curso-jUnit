da86b19e505af3856820f14b114c160ba5272842
Maria <m_isadora02@hotmail.com>
2021-04-19 21:46:25 -0300

    usando Rules

--- src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ce/wcaquino/servicos/LocacaoServiceTest.java
@@ -2,12 +2,12 @@ package br.ce.wcaquino.servicos;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.not;
-import static org.junit.Assert.assertThat;
 
 import java.util.Date;
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ErrorCollector;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -15,6 +15,10 @@ import br.ce.wcaquino.entidades.Usuario;
 import br.ce.wcaquino.utils.DataUtils;
 
 public class LocacaoServiceTest {
+
+	@Rule
+	// Anotacao que permite mudar o comportamento dos testes
+	public ErrorCollector error = new ErrorCollector();
 
 	@Test
 	public void TesteLocacao() {
@@ -27,12 +31,13 @@ public class LocacaoServiceTest {
 		// acao - execucao
 		Locacao locacao = service.alugarFilme(usuario, filme);
 
-		// validacao
-		// AssertThat = verifique que..
+		// verificacao
+		// Para o teste ser isolado deve criar 3 teste com cenario e ação para rastrear
+		// o erro
 
-		assertThat(locacao.getValor(), is(not(6.0)));
-		assertThat(locacao.getValor(), is(equalTo(5.0)));
-		assertThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
-		assertThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)), is(true));
+		error.checkThat(locacao.getValor(), is(equalTo(6.0)));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()), is(true));
+		error.checkThat(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)),
+				is(false));
 	}
 }
